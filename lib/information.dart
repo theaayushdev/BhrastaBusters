d53fc6dd858cb9f00f6fcc6f31f1958d3ae6be18
theaayushdev <theaayushdev07@gmail.com>
2025-06-13 11:59:11 +0545

    added important numbers

--- lib/information.dart
+++ lib/information.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Information extends StatelessWidget 
-{
+class NepalEmergencyInfoScreen extends StatelessWidget {
   final List<Map<String, String>> policeStations = [
     {'city': 'Kathmandu', 'number': '100 or 01-4200600'},
     {'city': 'Pokhara', 'number': '061-520000'},
@@ -10,13 +9,12 @@ class Information extends StatelessWidget
     {'city': 'Nepalgunj', 'number': '081-520000'},
   ];
 
-  final List<Map<String, String>> hospitals = [
-    {'city': 'Kathmandu', 'number': '01-4222000'},
-    {'city': 'Pokhara', 'number': '061-520000'},
-    {'city': 'Biratnagar', 'number': '021-523243'},
-    {'city': 'Butwal', 'number': '071-520000'},
-    {'city': 'Nepalgunj', 'number': '081-520000'},
-  ];
+  final Map<String, String> importantNumbers = {
+    'Ambulance': '102',
+    'Traffic Police': '104',
+    'Fire Brigade': '101',
+    'Women\'s Helpline': '1145',
+    'Child Helpline': '1098',
+    'Tourist Police': '1420',
+  };
 
-  
-}
