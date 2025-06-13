784e440b857447967d1166c8b9b628fe1275ff5c
Arya <aryadahal61@gmail.com>
2025-06-13 12:21:37 +0545

    Tried nepali to english conversion

--- Backend/credibility.py
+++ Backend/credibility.py
@@ -15,3 +15,29 @@ common_nepali_words = [
     "ghatana","ghatna","hunchha","huncha","hunechha","ghhus","ghus khori","loksewa","parhari"
 ]
 
+def is_romanized_nepali(text):
+    count = sum(1 for word in common_nepali_words if word in text.lower())
+    return count >= 2
+
+def smart_translate(text):
+    try:
+        lang = detect(text)
+        if lang == 'ne':
+            translated = translator.translate(text, dest='en').text 
+            return translated
+        
+    # If English but contains Nepali words
+        if lang == "en" and is_romanized_nepali(text):
+            try:
+                devanagari = transliterate(text, ITRANS, DEVANAGARI)
+                translated = translator.translate(devanagari, dest="en").text
+                return translated
+            except Exception :
+                 pass
+            
+         # If just English 
+        return translator.translate(text, dest="en").text
+            
+    except Exception:
+        return text 
+    
