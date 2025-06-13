821ac8940790476cefaa1c1faa4275ca29f0bcb6
theaayushdev <theaayushdev07@gmail.com>
2025-06-13 11:33:35 +0545

    added information section

--- lib/information.dart
+++ lib/information.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Information extends StatelessWidget 
+{
+  final List<Map<String, String>> policeStations = [
+    {'city': 'Kathmandu', 'number': '100 or 01-4200600'},
+    {'city': 'Pokhara', 'number': '061-520000'},
+    {'city': 'Biratnagar', 'number': '021-523243'},
+    {'city': 'Butwal', 'number': '071-520000'},
+    {'city': 'Nepalgunj', 'number': '081-520000'},
+  ];
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  final List<Map<String, String>> hospitals = [
+    {'city': 'Kathmandu', 'number': '01-4222000'},
+    {'city': 'Pokhara', 'number': '061-520000'},
+    {'city': 'Biratnagar', 'number': '021-523243'},
+    {'city': 'Butwal', 'number': '071-520000'},
+    {'city': 'Nepalgunj', 'number': '081-520000'},
+  ];
+
+  
 }
