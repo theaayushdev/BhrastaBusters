3aee008e12ade645e586ab975877f28f7ad044cc
Arya <aryadahal61@gmail.com>
2025-06-13 22:13:28 +0545

    fixes

--- Backend/app.py
+++ Backend/app.py
@@ -73,16 +73,3 @@ def submit_report():
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
 
-
-fnaflknasflknasf
-afanflkanflkanflkasf;alfknalfknaslkfnas
-fk;aflksanflkanfa
-lkaflkanfklsa
-fknkalfksafklas
-;kfnmalskfnaklsfna
-;klanmflkasnfkl;as
-;klfnmak;nfak;sfa'score_textlfnalskfn
-lkaknfsljafnlaksnfal
-lkansfklnalsff
-fmafkl;samflkasflka
-afnalkfnalkfnalknflaksnfklasf
