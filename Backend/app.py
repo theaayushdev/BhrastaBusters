b515b0f96c9c4ebd5e16b3ed3395b29b7bec26ff
Arya <aryadahal61@gmail.com>
2025-06-13 22:09:33 +0545

    fixes

--- Backend/app.py
+++ Backend/app.py
@@ -74,17 +74,3 @@ if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
 
 
-
-afafnalkfnsalkfnalfkna
-famf;lamsf;klaf;kamsfk;alsfk;af;asf
-afa;klklfaskfma
-fa;fmalskfalfa
-'fa';falmfa
-famfa'fa
-fa'fa'formatafaf
-af
-allfa
-finallyf
-af
-af
-allfaw
