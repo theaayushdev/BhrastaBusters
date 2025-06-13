a27234b30f5aa46053df1ac5e3d751512dcd20be
Arya <aryadahal61@gmail.com>
2025-06-13 22:13:13 +0545

    fixes

--- Backend/app.py
+++ Backend/app.py
@@ -74,3 +74,15 @@ if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
 
 
+fnaflknasflknasf
+afanflkanflkanflkasf;alfknalfknaslkfnas
+fk;aflksanflkanfa
+lkaflkanfklsa
+fknkalfksafklas
+;kfnmalskfnaklsfna
+;klanmflkasnfkl;as
+;klfnmak;nfak;sfa'score_textlfnalskfn
+lkaknfsljafnlaksnfal
+lkansfklnalsff
+fmafkl;samflkasflka
+afnalkfnalkfnalknflaksnfklasf
